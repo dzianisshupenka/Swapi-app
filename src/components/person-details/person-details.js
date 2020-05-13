24396ec67a577257a9c70e346ed303a0c37f6010
Dzianis <imxrayz@gmail.com>
2020-05-13 23:03:48 +0300

    add api to random planet componet

--- src/components/person-details/person-details.js
+++ src/components/person-details/person-details.js
@@ -11,9 +11,18 @@ export default class PersonDetails extends Component {
             <div>
                 <h4>Luke Skywalker</h4>
                 <ul className='list-group'>
-                    <li className='list-group-item list-group-item-action'>Gender: male</li>
-                    <li className='list-group-item list-group-item-action'>Birth Year: 55</li>
-                    <li className='list-group-item list-group-item-action'>Eye color: red</li>
+                    <li className='list-group-item list-group-item-action'>
+                        <span className='bolder'>Gender </span>
+                        <span >male</span>
+                    </li>
+                    <li className='list-group-item list-group-item-action'>
+                        <span className='bolder'>Birth Year </span>
+                        <span>55</span>
+                    </li>
+                    <li className='list-group-item list-group-item-action'>
+                        <span className='bolder'>Eye color </span>
+                        <span>red</span>
+                    </li>
                 </ul>
             </div>
         </div>
--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,19 +1,65 @@
 import React, {Component} from 'react';
+import SwapiService from "../../service/swapi-service";
 
 import './random-planet.css'
 
 export default class RandomPlanet extends Component {
+
+    swapiService = new SwapiService();
+
+    state = {
+        id: null,
+        name: null,
+        population: null,
+        rotationPeriod: null,
+        diameter: null
+    }
+
+    updatePlanet () {
+        const id = Math.floor(Math.random()*25) + 2;
+        this.swapiService.getPlanet(id)
+            .then((planet) => {
+                this.setState({
+                    id,
+                    name: planet.name,
+                    population: planet.population,
+                    rotationPeriod: planet.rotation_period,
+                    diameter: planet.diameter
+                })
+            })
+    }
+
+    constructor () {
+        super();
+        this.updatePlanet();
+    }
+
+
+
+
     render() {
+
+        const { id, name, population, rotationPeriod, diameter } = this.state;
+
         return <div className='random-planet'>
             <div>
-                <img src="https://starwars-visualguide.com/assets/img/planets/4.jpg" alt="planet 4"/>
+                <img src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} alt="planet 4"/>
             </div>
             <div>
-                <h4>Planet name</h4>
+                <h4>{name}</h4>
                 <ul className='list-group'>
-                    <li className='list-group-item list-group-item-action'>Population: 1654684</li>
-                    <li className='list-group-item list-group-item-action'>Rotation period: 85</li>
-                    <li className='list-group-item list-group-item-action'>Diameter : 5895</li>
+                    <li className='list-group-item list-group-item-action'>
+                        <span className='bolder'>Population </span>
+                        <span>{population}</span>
+                    </li>
+                    <li className='list-group-item list-group-item-action'>
+                        <span className='bolder'>Rotation period </span>
+                        <span>{rotationPeriod}</span>
+                    </li>
+                    <li className='list-group-item list-group-item-action'>
+                        <span className='bolder'>Diameter </span>
+                        <span>{diameter}</span>
+                    </li>
                 </ul>
             </div>
         </div>
