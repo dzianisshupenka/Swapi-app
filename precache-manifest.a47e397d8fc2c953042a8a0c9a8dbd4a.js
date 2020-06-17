f33840a5bcae9e333c405c4a7aa910028fbb088e
Dzianis <imxrayz@gmail.com>
2020-06-17 16:02:26 +0300

    Updates

---
+++ precache-manifest.a47e397d8fc2c953042a8a0c9a8dbd4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8230e5fe011f2c732e6d55dee67162c5",
+    "url": "/Swapi-app/index.html"
+  },
+  {
+    "revision": "97134294a781f07b06cd",
+    "url": "/Swapi-app/static/css/main.d26ba645.chunk.css"
+  },
+  {
+    "revision": "f5b2f11925d136ae1491",
+    "url": "/Swapi-app/static/js/2.b5fb3f1b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Swapi-app/static/js/2.b5fb3f1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97134294a781f07b06cd",
+    "url": "/Swapi-app/static/js/main.f341e555.chunk.js"
+  },
+  {
+    "revision": "30e331f0c0131c3f0985",
+    "url": "/Swapi-app/static/js/runtime-main.f581d3e4.js"
+  }
+]);
